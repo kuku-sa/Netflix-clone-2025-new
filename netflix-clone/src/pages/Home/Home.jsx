94123a5089bda5ed51ce6f8297b5995e2611da27
Kuku <rmesfin50@gmail.com>
2025-09-09 23:54:39 +0300

    banner

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
-import Row from "../../components/Row/Row"
-import requests from "../../utils/requests"
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import Banner from '../../components/Banner/Banner';
+import Row from "../../components/Row/Row";
+import requests from "../../utils/requests";
+
 const Home = () => {
   return (
     <>
-      <Header />
+      
+      <Banner />
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Footer />
+      
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
