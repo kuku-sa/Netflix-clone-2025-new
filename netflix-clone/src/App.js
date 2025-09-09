784c593542cc4266e66154adde7b305cf0d61f0d
Kuku <rmesfin50@gmail.com>
2025-09-09 20:28:23 +0300

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,12 +1,42 @@
+import React, { useState } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import "./index.css";
+import "./components/Header/header.css";
+import "./components/Footer/footer.css";
 
-import './App.css';
+const App = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-function App() {
   return (
     <div className="App">
-      <h1>kukushaye</h1>        
+      {/* Header */}
+      <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
+      {/* Mobile Hamburger Menu */}
+      {menuOpen && (
+        <div className="mobile_menu">
+          <ul>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>My List</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+      )}
+
+      {/* Main Content */}
+      <main style={{ minHeight: "70vh", padding: "50px", textAlign: "center" }}>
+        <h1>Welcome to Netflix Clone</h1>
+        <p>This is your main content area. Add movies, banners, and carousels here.</p>
+      </main>
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
